859f5400214d5801781548ea756d64d1e974d090
Nilson Junior <neff019@gmail.com>
2026-04-14 11:29:06 -0300

    Atualização do deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://nilsonjr11.github.io/suade-digital-app/"
+  // O base deve ser apenas o nome do projeto no GitHub
+  base: "/saude-digital-app/", 
 })
