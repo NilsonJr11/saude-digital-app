1a42d60af0ff34404380b1b75abbfc45f3a43006
Nilson Junior <neff019@gmail.com>
2026-05-20 18:09:03 -0300

    fix: removendo basename de vez para a raiz da vercel

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    // Força o Vite a usar o esbuild (muito mais tolerante) em vez do lightningcss
     cssMinify: 'esbuild',
     chunkSizeWarningLimit: 1600,
   }
