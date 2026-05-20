b09c4e20b43e6fc31751f367104df573dcdcd105
Nilson Junior <neff019@gmail.com>
2026-05-20 12:17:29 -0300

    build: desativa travas do compilador e limpa imports

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // O base deve ser apenas o nome do projeto no GitHub
-  base: "/saude-digital-app/", 
-})
+  build: {
+    // Evita que avisos de variáveis não usadas travem a geração do build final
+    chunkSizeWarningLimit: 1600,
+  }
+});
