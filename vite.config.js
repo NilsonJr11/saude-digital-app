cf405de09ff3617f3fc1856e24965affa09c9730
Nilson Junior <neff019@gmail.com>
2026-05-22 15:02:42 -0300

    build: clean compile forcing router reset

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/react-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    cssMinify: 'esbuild',
-    chunkSizeWarningLimit: 1600,
-  }
-});
+  plugins: [react(), tailwindcss()],
+})
